c5c50f9d7b186f58146b1d390bc0dbcf33a4ecce
rafaxzx <rafa_xzx@hotmail.com>
2020-04-27 01:44:10 -0300

    Aula95 Inicializando o Banco de Dados

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -1,12 +1,39 @@
-//import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
+const contactTable = "contactTable";
 const idColumn = "idColumn";
 const nameColumn = "nameColumn";
 const emailColumn = "emailColumn";
 const phoneColumn = "phoneColumn";
 const imgColumn = "imgColumn";
 
-class ContacHelper {}
+class ContactHelper {
+  static final ContactHelper _instance = ContactHelper.internal();
+  factory ContactHelper() => _instance;
+  ContactHelper.internal();
+
+  Database _db;
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db;
+    } else {
+      _db = await initDb();
+      return _db;
+    }
+  }
+
+  Future<Database> initDb() async {
+    final dataBasesPath = await getDatabasesPath();
+    final path = join(dataBasesPath, "contacts.db");
+
+    return await openDatabase(path, version: 1,
+        onCreate: (Database db, int newerVersion) async {
+      await db.execute(
+          "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT, $phoneColumn TEXT, $imgColumn TEXT)");
+    });
+  }
+}
 
 class Contact {
   int id;
