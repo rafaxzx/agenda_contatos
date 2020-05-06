22beecf8287f77ea9a096489937f27de4ef3be14
rafaxzx <rafa_xzx@hotmail.com>
2020-05-06 17:54:35 -0300

    Commit para teste do notebook Lenovo com VsCode

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:agendacontatos/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
+//Comentário de TESTE do Git pelo notebook Lenovo
 void main() => runApp(MaterialApp(
       home: HomePage(),
       debugShowCheckedModeBanner: false,
