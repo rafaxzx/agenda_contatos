97d9e80b1bd811ec9c51102fcaad10fd171b1dc5
rafaxzx <rafa_xzx@hotmail.com>
2020-05-17 19:47:18 -0300

    Aula 101 - Iniciando a criação da Tela de Contato

---
+++ lib/ui/contact_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:agendacontatos/helpers/contact_helper.dart';
+
+class ContactPage extends StatefulWidget {
+  final Contact contact;
+
+  ContactPage({this.contact});
+
+  @override
+  _ContactPageState createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  Contact _editedContact;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.contact == null) {
+      _editedContact = Contact();
+    } else {
+      _editedContact = Contact.fromMap(widget.contact.toMap());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(_editedContact.name ?? "Novo Contato"),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+        child: Icon(Icons.save),
+        onPressed: () {},
+      ),
+    );
+  }
+}
