3805a55f25a7fe7b764f6108f75640ff6d5fb462
rafaxzx <rafa_xzx@hotmail.com>
2020-05-27 02:31:28 -0300

    Aula 109 - Ordenando os Contatos (Final do App)

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -4,6 +4,8 @@ import 'package:agendacontatos/helpers/contact_helper.dart';
 import 'package:agendacontatos/ui/contact_page.dart';
 import 'package:flutter/material.dart';
 
+enum OrderOptions { orderaz, orderza }
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -27,6 +29,17 @@ class _HomePageState extends State<HomePage> {
         title: Text("Contatos"),
         backgroundColor: Colors.red,
         centerTitle: true,
+        actions: <Widget>[
+          PopupMenuButton<OrderOptions>(
+            itemBuilder: (context) => <PopupMenuEntry<OrderOptions>>[
+              const PopupMenuItem<OrderOptions>(
+                  child: Text("Ordenar de A-Z"), value: OrderOptions.orderaz),
+              const PopupMenuItem<OrderOptions>(
+                  child: Text("Ordenar de Z-A"), value: OrderOptions.orderza)
+            ],
+            onSelected: _orderList,
+          )
+        ],
       ),
       backgroundColor: Colors.white,
       floatingActionButton: FloatingActionButton(
@@ -82,7 +95,6 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       onTap: () => _showOptions(context, index),
-//      onTap: () => _showContactPage(contact: listContacts[index]),
     );
   }
 
@@ -169,5 +181,21 @@ class _HomePageState extends State<HomePage> {
             },
           );
         });
+  }
+
+  void _orderList(OrderOptions result) {
+    switch (result) {
+      case OrderOptions.orderaz:
+        contacts.sort((a, b) {
+          return a.name.toLowerCase().compareTo(b.name.toLowerCase());
+        });
+        break;
+      case OrderOptions.orderza:
+        contacts.sort((a, b) {
+          return b.name.toLowerCase().compareTo(a.name.toLowerCase());
+        });
+        break;
+    }
+    setState(() {});
   }
 }
